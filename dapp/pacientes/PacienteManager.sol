11f42ac9e10464d42c7b7073a07d56c005476429
YoYolops <yohanhxh@gmail.com>
2025-09-11 14:11:02 -0300

    Adiciona mappings e revisa campo wallet struct paciente

--- dapp/pacientes/PacienteManager.sol
+++ dapp/pacientes/PacienteManager.sol
@@ -1,13 +1,21 @@
-contract PacienteManager {
-    struct Procedimento {
-        string nome;
-    }
 
-    struct PacienteStorage {
+struct Procedimento {
+    // Importar procedimento
+    string nome;
+}
+
+
+contract Pacientes {
+    mapping(address => bytes32[]) private pacienteIdentificadores;
+    mapping(bytes32 => address) idToPaciente;
+
+    struct Paciente {
         address id;
         string nome;
         uint256 dataNascimento;
         bool ativo;
         Procedimento[] procedimentos;
+        bytes32[] wallets;
     }
 }
+
